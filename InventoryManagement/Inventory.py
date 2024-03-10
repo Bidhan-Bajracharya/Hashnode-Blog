d7adf279a9c003ad4f2d7e1a2e22c179c34f30aa
Bidhan <bidhanbajracharya01@gmail.com>
2024-03-10 15:40:12 +0545

    wip: inventory CRUD

--- InventoryManagement/Inventory.py
+++ InventoryManagement/Inventory.py
@@ -6,17 +6,42 @@ class Inventory:
         return f"Interesting, I forgot about this"
 
     def createItem(self, name, quantity, price):
-        self.items[name] = {'quantity': quantity, 'price': price}
+        if name not in self.items:
+            self.items[name] = {'quantity': quantity, 'price': price}
+            return "Item created successfully!"
+        else:
+            return f"Item with name '{name}' already exists!"
 
     def addItem(self, name, quantity):
         if name in self.items:
             self.items[name]['quantity'] += quantity
-            print("Quantity updated successfully!")
+            return "Quantity updated successfully!"
         else:
-            print(f"No item with name '{name}' exists!")
+            return f"No item with name '{name}' exists!"
 
     def findItem(self, name):
         if name in self.items:
-            print(self.items[name])
+            item = self.items[name]
+            return f"Item name: {name}, Available Quantity: {item['quantity']}, Price: ${item['price']}"
         else:
-            print(f"No item with name '{name}' exists!")
+            return f"No item with name '{name}' exists!"
+
+    def udpateItem(self, name, newQuantity):
+        if name in self.items:
+            self.items[name]['quantity'] = newQuantity
+        else:
+            return f"No item with name '{name}' exists!"
+        
+    def deleteItem(self, name):
+        if name in self.items:
+            del self.items[name]
+            return f"Item '{name}' removed successfully!"
+        else:
+            return f"No item with name '{name}' exists!"
+    
+    def totalStock(self):
+        sum = 0
+        for item in self.items:
+            sum = sum + self.items[item]['quantity']
+        return f"There are in total {sum} items in stock."
+    
--- InventoryManagement/main.py
+++ InventoryManagement/main.py
@@ -1,5 +1,8 @@
 from Inventory import Inventory
 
 inventory = Inventory()
+inventory.createItem('Oreo', 50, 10)
+inventory.createItem('Cookie', 20, 10)
 
-print(inventory)
+print(inventory.findItem('Oreo'))
+print(inventory.totalStock())
